81cdf1a1b098f3aea3a8a267fab16a23e0da44bc
Muhindo-Galien <muhindogalien512@gmail.com>
2022-12-06 18:24:21 +0300

    news component updated

--- src/components/modules/Jobs.jsx
+++ src/components/modules/Jobs.jsx
@@ -7,39 +7,39 @@ const Jobs = () => {
       <h2 className='text-2xl md:text-3xl text-center text-white my-8 '>Looking for job ? <br />
       CPA compannies are hiring!</h2>
       <article className='question bg-gray-800 shadow-xl'>
-        <div className='flex felx-row justify-between'>
+        <div className='flex felx-row justify-between items-center'>
           <div>
             <h2  className='text-medium font-medium text-white'>Software Developer</h2>
             <h3  className='text-gray-300 text-medium text-sm'>Amazon | <span>Posted 4days ago</span></h3>
           </div>
-          <button className='text-cente bg-white py-0 px-2 text-sm font-medium rounded-3xl'>APPLY ON COMPANY SITE</button>
+          <button className='text-cente bg-white py-1 px-2 text-sm font-medium rounded-3xl'>APPLY ON COMPANY SITE</button>
         </div>
       </article>
       <article className='question bg-gray-800 shadow-xl'>
-        <div className='flex felx-row justify-between'>
+        <div className='flex felx-row justify-between items-center'>
           <div>
             <h2  className='text-medium font-medium text-white'>Software Developer</h2>
             <h3  className='text-gray-300 text-medium text-sm'>Amazon | <span>Posted 4days ago</span></h3>
           </div>
-          <button className='text-cente bg-white py-0 px-2 text-sm font-medium rounded-3xl'>APPLY ON COMPANY SITE</button>
+          <button className='text-cente bg-white py-1 px-2 text-sm font-medium rounded-3xl'>APPLY ON COMPANY SITE</button>
         </div>
       </article>
       <article className='question bg-gray-800 shadow-xl'>
-        <div className='flex felx-row justify-between'>
+        <div className='flex felx-row justify-between items-center'>
           <div>
             <h2  className='text-medium font-medium text-white'>Software Developer</h2>
             <h3  className='text-gray-300 text-medium text-sm'>Amazon | <span>Posted 4days ago</span></h3>
           </div>
-          <button className='text-cente bg-white py-0 px-2 text-sm font-medium rounded-3xl'>APPLY ON COMPANY SITE</button>
+          <button className='text-cente bg-white py-1 px-2 text-sm font-medium rounded-3xl'>APPLY ON COMPANY SITE</button>
         </div>
       </article>
-      <article className='question bg-gray-800 shadow-xl'>
-        <div className='flex felx-row justify-between'>
+        <article className='question bg-gray-800 shadow-xl'>
+        <div className='flex felx-row justify-between items-center'>
           <div>
             <h2  className='text-medium font-medium text-white'>Software Developer</h2>
             <h3  className='text-gray-300 text-medium text-sm'>Amazon | <span>Posted 4days ago</span></h3>
           </div>
-          <button className='text-cente bg-white py-0 px-2 text-sm font-medium rounded-3xl'>APPLY ON COMPANY SITE</button>
+          <button className='text-cente bg-white py-1 px-2 text-sm font-medium rounded-3xl'>APPLY ON COMPANY SITE</button>
         </div>
       </article>
       
