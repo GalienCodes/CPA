314341b33868c785af8adf939a08a8a8c6c9e386
Muhindo-Galien <muhindogalien512@gmail.com>
2022-12-06 18:29:35 +0300

    news component updated

--- src/components/modules/Become.jsx
+++ src/components/modules/Become.jsx
@@ -7,7 +7,7 @@ const Become = () => {
       <h2 className='text-2xl md:text-3xl  text-white my-8 '>Become a member of CPA</h2>
       <p className='text-center text-gray-300 mb-8 text-lg mx-4'>As if serving the world wasn;t already enticing enough, being a member of CPA brings various benifits to you as a business owner</p>
       <div>
-        <div className='flex flex-row gap-4 justify-center items-center mb-2 m-4'>
+        <div className='flex flex-row gap-8 justify-center items-center mb-2 m-4'>
           <img src={fund} alt="fund" />
           <div>
             <h3 className='text-lg font-medium text-white'>Fundraising</h3>
@@ -15,24 +15,24 @@ const Become = () => {
                consectetur adipisicing elit. Exercitationem ex id sit ratione non aspernatur!</p>
           </div>
         </div>
-        <div className='flex flex-row gap-4 justify-center items-center mb-4 m-2'>
-          <img src={fund} alt="fund" />
+        <div className='flex flex-row gap-8 justify-center items-center mb-4 m-2'>
+          <img src={fund} alt="fund"  className='pr-2'/>
           <div>
             <h3 className='text-lg font-medium text-white'>Fundraising</h3>
             <p className='text-gray-300 text-medium text-sm'>Lorem, ipsum dolor sit amet
                consectetur adipisicing elit. Exercitationem ex id sit ratione non aspernatur!</p>
           </div>
         </div>
-        <div className='flex flex-row gap-4 justify-center items-center mb-6 m-2'>
-          <img src={fund} alt="fund" />
+        <div className='flex flex-row gap-8 justify-center items-center mb-6 m-2'>
+          <img src={fund} alt="fund"  className='pr-2'/>
           <div>
             <h3 className='text-lg font-medium text-white'>Fundraising</h3>
             <p className='text-gray-300 text-medium text-sm'>Lorem, ipsum dolor sit amet
                consectetur adipisicing elit. Exercitationem ex id sit ratione non aspernatur!</p>
           </div>
         </div>
-        <div className='flex flex-row gap-4 justify-center items-center mb-8 m-4'>
-          <img src={fund} alt="fund" />
+        <div className='flex flex-row gap-8 justify-center items-center mb-8 m-4 '>
+          <img src={fund} alt="fund" className='pr-2'/>
           <div>
             <h3 className='text-lg font-medium text-white'>Fundraising</h3>
             <p className='text-gray-300 text-medium text-sm'>Lorem, ipsum dolor sit amet
