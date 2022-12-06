1cda6e64ee54b5bca5d34f5ac20df5184c5c4d88
Muhindo-Galien <muhindogalien512@gmail.com>
2022-12-06 17:56:02 +0300

    news component added

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Become from './modules/Become'
 import Fqa from './modules/fqa/Fqa'
 import HeroPage from './modules/HeroPage'
+import Jobs from './modules/Jobs'
 import Members from './modules/Members'
 import News from './modules/News'
 import Profits from './modules/profits/Profits'
@@ -15,6 +16,7 @@ const LandingPage = () => {
       <Become/>
       <Fqa/>
       <News/>
+      <Jobs/>
     </>
   )
 }
---
+++ src/components/modules/Jobs.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import './fqa/fqa.css'
+
+const Jobs = () => {
+  return (
+    <div className='max-w-2xl flex flex-col  mx-auto justify-center items-center font-globalFont px-2 md:px-0'>
+      <h2 className='text-2xl md:text-3xl text-center text-white my-8 '>Looking for job ? <br />
+      CPA compannies are hiring!</h2>
+      <article className='question bg-gray-800 shadow-xl'>
+        <div className='flex felx-row justify-between'>
+          <div>
+            <h2  className='text-medium font-medium text-white'>Software Developer</h2>
+            <h3  className='text-gray-300 text-medium text-sm'>Amazon | <span>Posted 4days ago</span></h3>
+          </div>
+          <button className='text-cente bg-white py-0 px-2 text-sm font-medium rounded-3xl'>APPLY ON COMPANY SITE</button>
+        </div>
+      </article>
+      <article className='question bg-gray-800 shadow-xl'>
+        <div className='flex felx-row justify-between'>
+          <div>
+            <h2  className='text-medium font-medium text-white'>Software Developer</h2>
+            <h3  className='text-gray-300 text-medium text-sm'>Amazon | <span>Posted 4days ago</span></h3>
+          </div>
+          <button className='text-cente bg-white py-0 px-2 text-sm font-medium rounded-3xl'>APPLY ON COMPANY SITE</button>
+        </div>
+      </article>
+      <article className='question bg-gray-800 shadow-xl'>
+        <div className='flex felx-row justify-between'>
+          <div>
+            <h2  className='text-medium font-medium text-white'>Software Developer</h2>
+            <h3  className='text-gray-300 text-medium text-sm'>Amazon | <span>Posted 4days ago</span></h3>
+          </div>
+          <button className='text-cente bg-white py-0 px-2 text-sm font-medium rounded-3xl'>APPLY ON COMPANY SITE</button>
+        </div>
+      </article>
+      <article className='question bg-gray-800 shadow-xl'>
+        <div className='flex felx-row justify-between'>
+          <div>
+            <h2  className='text-medium font-medium text-white'>Software Developer</h2>
+            <h3  className='text-gray-300 text-medium text-sm'>Amazon | <span>Posted 4days ago</span></h3>
+          </div>
+          <button className='text-cente bg-white py-0 px-2 text-sm font-medium rounded-3xl'>APPLY ON COMPANY SITE</button>
+        </div>
+      </article>
+      
+    </div>
+  )
+}
+
+export default Jobs
