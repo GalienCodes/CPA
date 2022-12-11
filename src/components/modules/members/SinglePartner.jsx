3ce990118895ec9ae306a7b8a6eed01495cd7a60
Muhindo-Galien <muhindogalien512@gmail.com>
2022-12-12 00:02:39 +0300

    parteners modified properly!

--- src/components/modules/members/SinglePartner.jsx
+++ src/components/modules/members/SinglePartner.jsx
@@ -35,15 +35,16 @@ const SinglePartner = () => {
   };
 
   return (
-    <div className='max-w-2xl flex flex-col  mx-auto justify-center items-center font-globalFont px-4 md:px-0'>
+    <div className='max-w-2xl flex flex-col  mx-auto justify-center items-center font-globalFont mb-8 px-4 md:px-0'>
+      <h2 className='text-2xl md:text-3xl  text-white my-10 '>Meet some of our members</h2>
       <article className='review'>
         <div className="container-box">
           <div className='img-container flex-item-left'>
             <img src={image} alt={name} className='person-img' />
           </div>
           <div className='info flex-item-right'>
-            <h3>BOAS</h3>
-            <p >{text}</p>
+            <h3 className='company'>BOAS</h3>
+            <p className='details'>{text}</p>
           </div>
         </div>
           <div className='button-container '>
