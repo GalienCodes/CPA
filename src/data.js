0aa7658299cb405dad5f1312f543214cb5ba87f9
Muhindo Galien <74584848+Muhindo-Galien@users.noreply.github.com>
2023-01-08 23:19:36 +0300

    Update data.js

--- src/data.js
+++ src/data.js
@@ -13,12 +13,7 @@ const questions = [
     info:
       'Coloring book forage photo booth gentrify lumbersexual. Migas chillwave poutine synth shoreditch, enamel pin thundercats fashion axe roof party polaroid chartreuse.',
   },
-   {
-    id: 3,
-    title: 'Do I have to allow the use of cookies?',
-    info:
-      'Unicorn vinyl poutine brooklyn, next level direct trade iceland. Shaman copper mug church-key coloring book, whatever poutine normcore fixie cred kickstarter post-ironic street art.',
-  },
+
 ]
 
 export default questions
